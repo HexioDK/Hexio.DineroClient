8f444ef5b0feccb1760760a1eca84df1fa75ba9d
Morten Ricki Rasmussen <Mortyhd@live.dk>
2020-02-17 20:37:23 +0100

    Added more InvoiceLine Properties

--- Hexio.DineroClient/Models/Invoices/CreateInvoiceLineModel.cs
+++ Hexio.DineroClient/Models/Invoices/CreateInvoiceLineModel.cs
@@ -5,10 +5,12 @@ namespace Hexio.DineroClient.Models.Invoices
     public class CreateInvoiceLineModel
     {
         public Guid ProductGuid { get; set; }
+        public string Description { get; set; }
         public string Comments { get; set; }
         public decimal BaseAmountValue { get; set; }
         public string AccountNumber { get; set; } = "1000";
         public string Unit { get; set; } = "month";
         public int Quantity { get; set; }
+        public decimal Discount { get; set; }
     }
 }
---
+++ Hexio.DineroClient/Models/Invoices/InvoiceLineUnit.cs
@@ -0,0 +1,25 @@
+namespace Hexio.DineroClient.Models.Invoices
+{
+    public enum InvoiceLineUnit
+    {
+        Hours,
+        Parts,
+        Km,
+        Day, 
+        Week,
+        Month,
+        Kilogram,
+        CubicMetre,
+        Set,
+        Litre,
+        Box,
+        Case,
+        Carton,
+        Metre,
+        Package,
+        Shipment,
+        SquareMetre,
+        Session,
+        Tonne
+    }
+}
