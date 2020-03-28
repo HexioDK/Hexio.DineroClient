751364110ab29d3f9387be7d1b479cbdf28359c7
Morten Ricki Rasmussen <Mortyhd@live.dk>
2020-03-28 16:26:46 +0100

    Added Pagination resul to CollectionWrapper

--- Hexio.DineroClient/Models/CollectionWrapper.cs
+++ Hexio.DineroClient/Models/CollectionWrapper.cs
@@ -5,5 +5,15 @@ namespace Hexio.DineroClient.Models
     public class CollectionWrapper<T>
     {
         public IList<T> Collection { get; set; }
+        public Pagination Pagination { get; set; }
+    }
+
+    public class Pagination
+    {
+        public int MaxPageSizeAllowed { get; set; }
+        public int PageSize { get; set; }
+        public int Result { get; set; }
+        public int ResultWithoutFilter { get; set; }
+        public int Page { get; set; }
     }
 }
