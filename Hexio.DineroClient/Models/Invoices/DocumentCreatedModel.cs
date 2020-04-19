2666e6a8f3c06cc702428566573787731544dfbb
Morten Ricki Rasmussen <Mortyhd@live.dk>
2020-04-19 13:46:55 +0200

    Renamed InvoiceCreatedModel to DocumentCreatedModel

--- Hexio.DineroClient/Models/Invoices/DocumentCreatedModel.cs
+++ Hexio.DineroClient/Models/Invoices/DocumentCreatedModel.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Hexio.DineroClient.Models.Invoices
 {
-    public class InvoiceCreatedModel
+    public class DocumentCreatedModel
     {
         public Guid Guid { get; set; }
         public string TimeStamp { get; set; }
