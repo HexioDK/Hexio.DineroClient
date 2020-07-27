571c820c0f2ed73b926492eeba09e763138adf75
Morten Ricki Rasmussen <Morten@Hexio.dk>
2020-07-27 16:26:09 +0200

    More registered types in module

--- Hexio.DineroClient/Module/DineroModule.cs
+++ Hexio.DineroClient/Module/DineroModule.cs
@@ -1,21 +1,25 @@
 using Autofac;
 using Hexio.DineroClient.Auth;
+using Hexio.DineroClient.Authorization;
 
 namespace Hexio.DineroClient.Module
 {
     public class DineroModule : Autofac.Module
     {
         private readonly DineroApiSettings _dineroApiSettings;
+        private readonly VismaConnectSettings _vismaConnectSettings;
 
-        public DineroModule(DineroApiSettings dineroApiSettings)
+        public DineroModule(DineroApiSettings dineroApiSettings, VismaConnectSettings vismaConnectSettings)
         {
             _dineroApiSettings = dineroApiSettings;
+            _vismaConnectSettings = vismaConnectSettings;
         }
 
         protected override void Load(ContainerBuilder builder)
         {
             builder.Register(x => _dineroApiSettings);
-
+            builder.Register(x => _vismaConnectSettings);
+            
             if (_dineroApiSettings.GetType() == typeof(SingleDineroAccountApiSettings))
             {
                 builder.Register(x => (SingleDineroAccountApiSettings) _dineroApiSettings);
@@ -23,6 +27,10 @@ namespace Hexio.DineroClient.Module
             
             builder.Register(x => DineroAuthClientFactory.Execute(_dineroApiSettings)).SingleInstance().AsImplementedInterfaces();
             builder.Register(x => DineroClientFactory.Execute(_dineroApiSettings)).SingleInstance().AsImplementedInterfaces();
+            
+            builder.RegisterType<GetAuthorizeInfo>().AsImplementedInterfaces();
+            builder.RegisterType<GetEndpoints>().AsImplementedInterfaces();
+            builder.RegisterType<RequestAuthorizationToken>().AsImplementedInterfaces();
         }
     }
 }
