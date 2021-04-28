70048aba82814d056e3e8ae4630b43438eca4abd
Morten Ricki Rasmussen <Morten@Hexio.dk>
2021-04-28 14:21:43 +0200

    Added Fetch Dinero Totals

--- Hexio.DineroClient/IDineroClient.cs
+++ Hexio.DineroClient/IDineroClient.cs
@@ -90,6 +90,10 @@ namespace Hexio.DineroClient
         [Post("/v1/{organizationId}/invoices")]
         [AllowAnyStatusCode]
         Task<Response<DocumentCreatedModel>> CreateInvoice([Body] CreateInvoiceModel model);
+        
+        [Post("/v1/{organizationId}/invoices/fetch")]
+        [AllowAnyStatusCode]
+        Task<Response<DocumentCreatedModel>> CreateInvoiceTotals([Body] CreateInvoiceModel model);
 
         [Put("v1.2/{organizationId}/invoices/{invoiceGuid}")]
         [AllowAnyStatusCode]
